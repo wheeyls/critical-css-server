f2f2d262b322cb35804e159590b2bfb1509edefd
Michael Wheeler <mwheeler@g2crowd.com>
2017-01-29 22:35:34 -0600

    Parse redisurl for matador

--- src/bull-ui.js
+++ src/bull-ui.js
@@ -1,6 +1,11 @@
 var port = process.env.BULL_UI_PORT || 8080;
-console.log(process.env.REDIS_URL);
-var app = require('bull-ui/app')({ redis: { url: process.env.REDIS_URL } });
+var redisOpts = require('redis-url').parse(process.env.REDIS_URL);
+var app = require('bull-ui/app')({ redis: {
+  host: redisOpts.hostname,
+  port: redisOpts.port,
+  password: redisOpts.password,
+  options: { database: redisOpts.database } }
+});
 
 app.listen(port, function() {
   console.log('bull-ui started listening on port', this.address().port);
