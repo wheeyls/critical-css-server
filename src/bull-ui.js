25107cc4d59c2e514892ddda179dc2765e7b8397
Michael Wheeler <mwheeler@g2crowd.com>
2017-01-29 22:01:18 -0600

    Use foreman locally for dev

--- src/bull-ui.js
+++ src/bull-ui.js
@@ -1,5 +1,6 @@
+var port = process.env.BULL_UI_PORT || 8080;
 var app = require('bull-ui/app')({ redis: { url: process.env.REDIS_URL } });
 
-app.listen(1337, function(){
+app.listen(port, function() {
   console.log('bull-ui started listening on port', this.address().port);
 });
