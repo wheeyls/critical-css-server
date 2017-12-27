7464e2c16e219fba43e6960d1eeebbdbe7a1d1c5
Danial Knox <dknox@g2crowd.com>
2017-12-27 17:18:04 -0600

    Putting the processing into another file so that it runs in separate process within bull

--- src/workers.js
+++ src/workers.js
@@ -1,15 +1,12 @@
 // @format
-var BuildCss = require('./workers/BuildCss.js');
-var redis = require('redis');
-var generator = require('./generator.js');
+var path = require('path');
 
 module.exports = function(queue) {
   return {
     start: function() {
       console.log('bull: worker started...');
 
-      var redisClient = redis.createClient({url: process.env.REDIS_URL});
-      var worker = new BuildCss(generator(), redisClient);
+
 
       queue.on('completed', function(job) {
         console.log('bull: completed: ', job.data.page.key);
@@ -31,20 +28,7 @@ module.exports = function(queue) {
         console.log('bull: job failed! ', job);
       });
 
-      queue.process(function(job, done) {
-        console.log('bull: process started...', job.data.page.key);
-
-        return worker
-          .perform(job.data)
-          .then(function(data) {
-            console.log('bull: process done', job.data.page.key);
-            done(null, data);
-          })
-          .catch(function(err) {
-            console.log('bull: process catch', job.data.page.key);
-            done(err);
-          });
-      });
+      queue.process(path.join(process.cwd(),'src', 'processors', 'cssProcessor.js'));
     },
   };
 };
